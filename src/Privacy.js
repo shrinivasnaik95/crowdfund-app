ad8d4d7299acf3be9f8d746fa085fa20dc9f67f2
SHRINIVAS NAIK <naikshrinu8746@gmail.com>
2026-05-10 17:43:47 +0530

    Create Privacy.js

---
+++ src/Privacy.js
@@ -0,0 +1,20 @@
+export default function Privacy() {
+
+  return (
+    <div className="container mt-5">
+
+      <h2>Privacy Policy</h2>
+
+      <p>
+        CrowdFund respects user privacy and protects
+        personal information shared on the platform.
+      </p>
+
+      <p>
+        Payment information is securely processed
+        through trusted payment gateways.
+      </p>
+
+    </div>
+  );
+}
