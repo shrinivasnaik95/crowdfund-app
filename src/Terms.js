1cc6f4529a783a03e6ce02274bf883f37496d801
SHRINIVAS NAIK <naikshrinu8746@gmail.com>
2026-05-10 17:44:14 +0530

    Create Terms.js

---
+++ src/Terms.js
@@ -0,0 +1,20 @@
+export default function Terms() {
+
+  return (
+    <div className="container mt-5">
+
+      <h2>Terms & Conditions</h2>
+
+      <p>
+        Users are responsible for the authenticity
+        of campaigns created on the platform.
+      </p>
+
+      <p>
+        CrowdFund reserves the right to remove
+        fraudulent or misleading campaigns.
+      </p>
+
+    </div>
+  );
+}
