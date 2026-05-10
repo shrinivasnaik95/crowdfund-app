6b3ca1da057b829b472468f92dfe318a881b4e4c
SHRINIVAS NAIK <naikshrinu8746@gmail.com>
2026-05-10 17:43:17 +0530

    Create Contact.js

---
+++ src/Contact.js
@@ -0,0 +1,18 @@
+export default function Contact() {
+
+  return (
+    <div className="container mt-5">
+
+      <h2>Contact Us</h2>
+
+      <p>Email: naikshrinu8746@gmail.com</p>
+
+      <p>Phone: +91 9353084595</p>
+
+      <p>
+        We usually respond within 24 hours.
+      </p>
+
+    </div>
+  );
+}
